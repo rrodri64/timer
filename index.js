f5a0c34702abd2d3a1aded945c884cf0f6abe3a5
Rebecca Rodriguez <rerodriguez@protonmail.com>
2023-10-11 10:53:37 -0700

    Added working start button and pause button functionality that prints to the console

--- index.js
+++ index.js
@@ -5,15 +5,25 @@ class Timer {
     this.pauseButton = pauseButton;
 
     this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
   }
 
-  start() {
-    console.log("Time to start the timer!");
+  start = () => {
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
   }
+
+  tick = () => {
+    console.log("tick");
+  };
 }
 
-const durationInput =  document.querySelector('#duration');
-const startButton =  document.querySelector('#start');
-const pauseButton =  document.querySelector('#pause');
+const durationInput = document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
 
 const timer = new Timer(durationInput, startButton, pauseButton);
